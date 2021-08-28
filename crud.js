790b46b8f7026ed333ed698d0b3e6c04c657305e
geevar007 <68139937+geevar007@users.noreply.github.com>
2021-08-28 11:54:44 +0530

    Update crud.js

--- crud.js
+++ crud.js
@@ -1,33 +1,33 @@
-/*<script type="text/javascript">*/
-
-//Add your app script url here
-// On each update in App Script You need to update url here.
-
-
 var url = "https://script.google.com/macros/s/AKfycbzIukcrWtqF8AadmIkce4IchaeSyvcep18qZhjHFPOE6MHgLSo/exec";
-
 var gx= new Date();
-  
 var m = gx.getMonth();
-
 var y= gx.getFullYear();
 var dd=gx.getDate();
-var response;
-
+var sData;
+var month;
+var year;
 m++;
 
   $("#monthSelect").val(m);
   $("#yearSelect").html(y);
- 
+ //-----------------------------------------------------------------document ready-------------------------
   $(document).ready(function(){  
- 
+    document.getElementById("printRep").style.display="none";
+    document.getElementById("itemTable").style.display="none";
+localStorage.clear();
+month =$( "#monthSelect option:selected" ).text();
+year = $("#yearSelect").text();
+gRefresh();
 
+});
+//--------------------------------------------------------------document ready End here-----------------------
 
+//--------------------------------------------------------------multi dates submit-------------------------
     $("#btn_add_dates").click(function(){
 
       var dates =$("#monitor").val();
-      var month=$( "#monthSelect option:selected" ).text();
-      var year= $("#yearSelect").text();
+       month=$( "#monthSelect option:selected" ).text();
+      year= $("#yearSelect").text();
 if(dates==""){alert("Nothing to add")}
 
 else{
@@ -45,17 +45,19 @@ else{
     function(data,status){
       alert(data);
       $body.removeClass("loading");
-     gRefresh();
+    check(data);
+       
+    
     
   });
 }
     })
+//-------------------------------------------------------------multi Date submit End-------------------------
 
 
 
 
-
-  /*-------------------------------------------------- Add Item Operation --------------------------------------------*/
+  /*---------------------------------------------------------- Add Item submit --------------------------*/
    $("#addItemform").submit(function(event) {
       event.preventDefault();
                 /* stop form from submitting normally */
@@ -66,8 +68,8 @@ else{
       var to = $("#to").val();
       var purpose = $("#purpose").val();
       var km = $("#km").val();
-      var month=$( "#monthSelect option:selected" ).text();
-      var year= $("#yearSelect").text();
+      //var month=$( "#monthSelect option:selected" ).text();
+      //var year= $("#yearSelect").text();
       if ($('#mode').is(":checked")){var mode="Bus"}
     else{var mode="Dept:Jeep"}
      
@@ -95,22 +97,17 @@ else{
       function(data,status){
         alert(data);
         $body.removeClass("loading");
-       gRefresh();
+       check(data);
       
     });
   });
   
-  /* --------------------------------------------------End Of Add Item Operation----------------------------------------------------- */
+  /* ---------------------------------------------------------Add item submit End---------------------- */
   
-  /* ------------------------------------------------------Read Operation ------------------------------------------*/
-  
-  gRefresh();
-
-
-/*------------------------------------------------------ End of Read Operation------------------- */  
+ 
   
 
-//----------------------------------------------------------mobile edit starts -----------------------------
+//-------------------------------------------------------------mobile edit starts -------------------
 
 $(document).on('click', '.meditItem', function(){  
 
@@ -149,10 +146,10 @@ var timea= x[0];
 
 
 });
-//-----------------------------------------------------------mobile edit End ---------------------
+//-------------------------------------------------------------mobile edit End ---------------------
 
 
-//----------------------------------------------------------mobile Deletec starts----------------------------------
+//-------------------------------------------------------------mobile Deletec starts-----------------------
 
 $(document).on('click', '.mdeleteItem', function(){  
            
@@ -172,10 +169,10 @@ $(document).on('click', '.mdeleteItem', function(){
 
   });
 
-//--------------------------------------------------------mobile Delete End--------------------------------------------------
+//------------------------------------------------------------mobile Delete End-------------------------
 
   
-  /* --------------------------------------------------------- Edit start--------------------------------*/ 
+  /* ----------------------------------------------------------- Edit start--------------------------------*/ 
 
    $(document).on('click', '.editItem', function(){  
            
@@ -212,9 +209,9 @@ $(document).on('click', '.mdeleteItem', function(){
       
   
   });
+  //-------------------------------------------------------------Endit End------------------------------
   
-  
-  
+  //-------------------------------------------------------------edit Submit ---------------------------
    $("#editItemForm").submit(function(event) {
                event.preventDefault();
 
@@ -227,8 +224,8 @@ $(document).on('click', '.mdeleteItem', function(){
       
       var km = $("#editkm").val();
       var purpose = $("#editpurpose").val();
-      var month =$( "#monthSelect option:selected" ).text();
-      var year = $("#yearSelect").text();
+      //var month =$( "#monthSelect option:selected" ).text();
+      //var year = $("#yearSelect").text();
       if ($('#eMode').is(":checked")){var mode="Bus"}
     else{var mode="Dept:Jeep"}
 
@@ -245,11 +242,8 @@ $(document).on('click', '.mdeleteItem', function(){
         action: "updateItem",
        no:no,
         date:date,
-       // month:month,
-       // year:year,
-        timea:timea+" to "+timeb,
-       
-        from:from,
+       timea:timea+" to "+timeb,
+       from:from,
         to:to,
         mode:mode,
         km:km,
@@ -262,18 +256,12 @@ $(document).on('click', '.mdeleteItem', function(){
         
       $body.removeClass("loading");
      alert(data); 
-
-       // location.reload();
-       gRefresh();
+     check(data);
     });
   });
+  //-------------------------------------------------------------edit Submit End---------------------------
 
-
-
-	/*------------------------------------------------ End of Edit / Update operationb -----------------------*/
-	
-	
-	/*------------------------------------------------------------table Delete operation ------------------------------*/
+	/*------------------------------------------------------------table Delete operation -----------------*/
 
    $(document).on('click', '.deleteItem', function(){  
            
@@ -292,14 +280,14 @@ $(document).on('click', '.mdeleteItem', function(){
       
   
 		});
-
-
+//---------------------------------------------------------------table Delete End----------------------------
+//--------------------------------------------------------------table delete Submit Stars-------------------
    $("#deleteItemForm").submit(function(event) {
                event.preventDefault();
       
       var no = $("#deleteno").html();
       
-      var month =$( "#monthSelect option:selected" ).text();
+      //var month =$( "#monthSelect option:selected" ).text();
   
 
     //  alert(itemName);
@@ -318,19 +306,21 @@ $(document).on('click', '.mdeleteItem', function(){
       function(data,status){
        $body.removeClass("loading");
         alert(data);
-        gRefresh();
-        //location.reload();
+        check(data);
+        
+      
     });
   });
+  //-----------------------------------------------------------table Delete submit End here--------------
   
-  
-  /*-------------------------------------- End  table Delete operation -----------------------------------------*/
+
   
   $(document).on('click', '#prev', function(){ 
 
 
     y--;
 $("#yearSelect").html(y);
+year=y;
 gRefresh();
 
   })
@@ -340,42 +330,64 @@ gRefresh();
 
     y++;
 $("#yearSelect").html(y);
+year=y;
 gRefresh();
   })
-//------------------------------------------gRefresh-----------------------------------------------------------
+//-------------------------------------------------------------gRefresh------------------------------------
 
-function gRefresh()
-{   $("#disMonth").html($( "#monthSelect option:selected" ).text());
+function gRefresh(){
+$("#disMonth").html($( "#monthSelect option:selected" ).text());
 $("#disYear").html($("#yearSelect").text());
 $("#edisMonth").html($( "#monthSelect option:selected" ).text());
 $("#edisYear").html($("#yearSelect").text());
  $("#date").val(dd);
-  $( "#mobileViewArea" ).load(window.location.href + " #mobileViewArea" );
-  $( "#itemTable" ).load(window.location.href + " #itemTable" );
-  var month =$( "#monthSelect option:selected" ).text();
-  var year = $("#yearSelect").text();
-  $("#calenderTitle").html(month+" "+year);
+ $( "#mobileViewArea" ).load(window.location.href + " #mobileViewArea" );
+ $( "#itemTable" ).load(window.location.href + " #itemTable" );  
+$("#calenderTitle").html(month+" "+year);
   
   $body = $("body");
-  $body.addClass("loading"); 
+
   
-    jQuery.get(url+"?action=getItems&month="+month+"&year="+year, function(data, status){
-    
+  if(localStorage.getItem(year+month)){
+    console.log(month+year+" Data is here");
+    var gPassD = JSON.parse(localStorage.getItem(year+month));
+setTimeout(function() {
+      drawTables(gPassD); 
+    }, 300);
+  
+  }
+  else{
+$body.addClass("loading"); 
+    fetchData();
+  }
+  
+  $("#monitor").val("");
+ 
+    }
+//------------------------------------------------------------gRefresh End here---------------------------
 
-    if(data.tableEmpty == 'N'){  
+function fetchData(){
+  
+  jQuery.get(url+"?action=getItems&month="+month+"&year="+year, function(data, status){
+    sData=JSON.stringify(data);
+    localStorage.setItem(year+month,sData);
+   drawTables(data);
+    $body.removeClass("loading");
+
+  });
+};
+
+  //-----------------------------------------------------------draw Tables function--------------------
+  function drawTables(data) { 
+   if(data.tableEmpty == 'N'){  
     document.getElementById("printRep").style.display="block";
-     document.getElementById("itemTable").style.display="block"; 
-     document.getElementById("noRImg").style.display="none"; 
-    response = data.items;
-    $(function () { 
-     
-    $.each(response, function (i, item) { 
-      
-      
-
+       document.getElementById("itemTable").style.display="block"; 
+       document.getElementById("noRImg").style.display="none"; 
+     var response = data.items;
+    
+      $.each(response, function (i, item) { 
             $(
               "<tr>"+
-              
               "<td>"+item.no+"</td>"+
               "<td>"+item.date+"</td>"+
               "<td>"+item.timea+"</td>"+
@@ -387,40 +399,25 @@ $("#edisYear").html($("#yearSelect").text());
               "<td>"+
                  '<a href = "" class="editItem" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">edit</i></a>'+
                  '<a href = "" class="deleteItem" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>'+
-              
-              
-
-              "</td>"+
+                "</td>"+
             "</tr>").appendTo('#itemTable');
-          
-          var timeMix=item.timea;
+var timeMix=item.timea;
 var x =timeMix.split(" to ");
 
             $(
 '<div class="mySlip"><div id="dDate">'+
         '<p id="pMonth">'+item.month+'</p><h2>'+item.date+'</h2><p id="pYear">'+item.year+'</p></div>'+
-   
-
-
-        '<div class=dData>'+                       
+    '<div class=dData>'+                       
         '<p><b>Time:</b> '+x[0]+' <b>to:</b> '+x[1]+'</p>'+
         '<p><b>From: </b>'+item.from+' <b>To:</b> '+item.to+'</p>'+
          '<p><b>Purpose: </b>'+item.purpose+'</p>'+
          '<p><b>KM: </b>'+item.km+', <b>Mode: </b>'+item.mode+'</p></div>'+
-        
-
-
-
-
-
-
-    '</div>'+
+      '</div>'+
 
  '<div class="dfoot">'+
  '<P >'+item.no+'</p>'+
  '<p hidden>'+item.date+'</p>'+
  '<p hidden>'+item.timea+'</p>'+
- 
  '<p hidden>'+item.from+'</p>'+
  '<p hidden>'+item.to+'</p>'+
  '<p hidden>'+item.purpose+'</p>'+
@@ -428,76 +425,66 @@ var x =timeMix.split(" to ");
  '<p hidden>'+item.km+'</p>'+
  '<a href = "" class="meditItem" data-toggle="modal"><i class="material-icons mEditbtn" data-toggle="tooltip" title="Edit">edit</i></a>'+
                  '<a href = "" class="mdeleteItem" data-toggle="modal"><i class="material-icons mDeletebtn" data-toggle="tooltip" title="Delete">&#xE872;</i></a>'+
-
-
-
-'</div>'
+  '</div>'
   ).appendTo('#mobileViewArea')});
+ }
+ else{
+  // dates=[];
+  console.log("no data for selected month"+month);
+document.getElementById("printRep").style.display="none";
+  document.getElementById("itemTable").style.display="none";
+  document.getElementById("noRImg").style.display="block"; 
+ $body.removeClass("loading"); 
 
- });// -----------------------------function inside jquory end here--------------------------
+ }
 
 
 
-
-    $body.removeClass("loading");
-    }//-------------------------------if table empty  end here--------------
-    else{
-     // dates=[];
-   document.getElementById("printRep").style.display="none";
-     document.getElementById("itemTable").style.display="none";
-     document.getElementById("noRImg").style.display="block"; 
-    $body.removeClass("loading"); 
-
-    }
-    $("#monitor").val("");
-    
-   
-  
-  });//------------------------------j quory end here---------------
-  
-  
-
- 
-}//-------------------------------------------gRefresh End here-------------------------------------------------------
-
+}// ------------------------------------------------------------end function draw Tables -----------------
 
 $("#monthSelect").change(function(){
-  var month =$( "#monthSelect option:selected" ).text();
+   month =$( "#monthSelect option:selected" ).text();
   
   alert("view entries in "+month+" only");
  gRefresh();
 });
 
-  
-});
+//---------------------------------------------------------------  
 
 $("#fromI").click(function(){
 $('#from').val("Thrissur HQ");});
-
+//--------------------------------------------------------------
 $("#toI").click(function(){
   $('#to').val("Thrissur HQ");});
-
+//--------------------------------------------------------------
   $("#purI").click(function(){
     $('#purpose').val("Assisting SSO for");});
-
+//-----------------------------------------------------------------
    
    
-    
+ //----------------------------------multi date  buten press starts--------------   
     function gpress(x) {
  
    var y =$("#monitor").val();
 $("#monitor").val(y+x+"x");
+ }//---------------------------------multi date  buten press End--------------------
 
 
-    }
-console.log(response);
+//---------------------------------------Print press Starts----------------
 function save(){
-      var value= JSON.stringify(response);
-     console.log(value);
-      var passData = "?" +value ;
-      window.location.href = "save.html" + passData;}
+      window.location.href = "save.html" +"?" +localStorage.getItem(year+month);}
+//----------------------------------------print press Ends-------------------------
 
+
+
+//-------------------------------------Reading key- function starts---------------------
 function readKey(){
       var gx =$("#myKey").val();
         return gx;
- }
+ } 
+ //-------------------------------------Reading key- function End---------------------
+ function check(data)
+ {if(data!="Call Geevar"){
+  console.log( "data is"+data);
+  localStorage.removeItem(year+month);
+  gRefresh();}}
