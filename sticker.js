738224907cc6cf48fc01f75068cd19b1a014ab5a
geevar007 <68139937+geevar007@users.noreply.github.com>
2023-06-13 16:01:03 +0530

    Update sticker.js

--- sticker.js
+++ sticker.js
@@ -4,19 +4,26 @@ document.getElementById("printing").style.display="none"
 document.getElementById("messagebox").style.display="none"
 document.getElementById("canselB").style.opacity="10%";
 var gOutPut=0;
-function enter() { 
+
+updateTime();
+
+
+function enter() { updateTime();
+    var printName="";
+    var printNo="";
     document.getElementById("printB").style.opacity="10%";
     var samplName= document.getElementById("sampleCode").value;
     $('#paper').load(document.URL + ' #paper');
     var x =samplName;
     
     setTimeout(function(){
-    if (x == "" || x == null) {
-
+    if (x == " " || !x ) {
+        document.getElementById('sTime').style.display="none";
         document.getElementById("messagebox").style.display="block";
 
         setTimeout(function(){ 
             document.getElementById("messagebox").style.display="none"
+            document.getElementById('sTime').style.display="block";
 
         },800);
         
@@ -27,21 +34,31 @@ else {
     var endNumber= document.getElementById("endNo").value;
     var names = document.getElementsByClassName("name");
     var codes = document.getElementsByClassName("code");
-    var no= Number(sampleNumber);
+    var exno= no= Number(sampleNumber);
     var eNo= Number(endNumber);
-    console.log("ending number is ="+eNo);
+    var position =document.getElementById("locat").value;
+    var status="Start"; 
+  
     
     
 gOutPut=1;
 
     for(var i=0;i<27;i++){
 
-if(no==0){no=""};
+
+if (position) { 
+                 if (i==position-1) {printName=samplName; printNo  = exno;no = exno;status="Stop"}  
+                else{  printNo = no;  if (status=="Start"){ printNo=""} }
+                }
+ else {   printNo = no; if (status=="Start") {  printName=samplName;} }
+
+
+if(no==0){printNo=""};
 
 $(
 
 
-"<div class=sticker>"+"<div class=printArea>"+ "<h3 class=name>"+samplName+"</h3>"+"<p class=code>"+no+"</p>"+"</div>"+"</div>"
+"<div class=sticker>"+"<div class=printArea>"+ "<h3 class=name>"+printName+"</h3>"+"<p class=code>"+printNo+"</p>"+"</div>"+"</div>"
 
 
 
@@ -53,28 +70,40 @@ document.getElementById("posPaper").style.display="block";
 document.getElementById("canselB").style.opacity="100%";
 
 if (no!=0){no++;};
-if( eNo+1==no){no=0};
+if( eNo+1==no ){no=0; if(document.querySelector('#endPrint').checked){ printName="";status="Stop"}     };
 
 };
 
  }},100);}
 
- function gPrint(){
+ function gPrint(){updateTime()
  if (gOutPut!=0){
    document.getElementById("enterB").style.opacity="100%";
     document.getElementById("posPaper").style.display="none";
+    document.getElementById('sTime').style.display="none";
     document.getElementById("printing").style.display="block";
     
     setTimeout(function()
-    {document.getElementById("printing").style.display="none"},1800);
+    {document.getElementById("printing").style.display="none";
+    document.getElementById('sTime').style.display="block";},1800);
 
     setTimeout(function()
     {window.print()},850);}}
 
 
- function gCansel(){
+ function gCansel(){ updateTime()
 document.getElementById("posPaper").style.display="none";
 document.getElementById("canselB").style.opacity="10%";
 document.getElementById("enterB").style.opacity="100%";
  }
- 
+ function updateTime(){
+    var d = new Date(); //object of date()
+	var hr = d.getHours();
+	var min = d.getMinutes();
+    if (hr>12)
+    { hr = hr-12;
+    min= min+" PM"}
+    if (hr==0) {hr=12}
+   document.getElementById('sTime').innerHTML= hr+":"+min;
+}
+
